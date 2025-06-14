27900ae3d0238573bf7a1d84ce39d9c425a12c37
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-06-14 09:55:42 +0000

    com

--- 04-dfs/dfs.cpp
+++ 04-dfs/dfs.cpp
@@ -1,26 +1,26 @@
-class Solution {
-public:
-    void dfsUtil(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ls) {
-        vis[node] = 1;
-        ls.push_back(node);
+// class Solution {
+// public:
+//     void dfsUtil(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ls) {
+//         vis[node] = 1;
+//         ls.push_back(node);
 
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                dfsUtil(it, adj, vis, ls);
-            }
-        }
-    }
+//         for (auto it : adj[node]) {
+//             if (!vis[it]) {
+//                 dfsUtil(it, adj, vis, ls);
+//             }
+//         }
+//     }
 
-    vector<int> dfs(vector<vector<int>>& adj) {
-        int n = adj.size();
-        vector<int> vis(n, 0);
-        vector<int> ls;
+//     vector<int> dfs(vector<vector<int>>& adj) {
+//         int n = adj.size();
+//         vector<int> vis(n, 0);
+//         vector<int> ls;
 
-        int start = 0;
-        dfsUtil(start, adj, vis, ls);
+//         int start = 0;
+//         dfsUtil(start, adj, vis, ls);
 
-        return ls;
-    }
-};
-//sc : O(n)[traversing n nodes ] + O(n) [visited] + O(n)[recursion stack space]
-//tc : O(n) + O(2E)
+//         return ls;
+//     }
+// };
+// //sc : O(n)[traversing n nodes ] + O(n) [visited] + O(n)[recursion stack space]
+// //tc : O(n) + O(2E)
